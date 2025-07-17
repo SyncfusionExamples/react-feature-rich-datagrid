73fcedf7817e0c1bcadbac9431de1bdb9454509e
indumathi-murali <57896631+indumathi-murali@users.noreply.github.com>
2025-07-17 15:12:54 +0530

    Committted changes.

--- src/common/index.tsx
+++ src/common/index.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { createRoot } from 'react-dom/client';
 import { App } from "../components/Grid/App";
-import "../../styles/index.css";
 import "../components/Grid/App.css";
 import { registerLicense } from '@syncfusion/ej2-base';
 registerLicense('YOUR LICENSE KEY');
